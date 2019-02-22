2c332a2501586a74aef8c318bcd3ec996badc83f
bohlscf <44482641+bohlscf@users.noreply.github.com>
2019-02-22 14:52:18 -0500

    Update Joystick_LED_Directions.ino

--- Joystick_LED_Directions.ino
+++ Joystick_LED_Directions.ino
@@ -1,13 +1,13 @@
 
-int pin12 = 12;   //OUTPUT: show movement to the right
-int pin11 = 11;   //OUTPUT: show movement upwards
-int pin10 = 10;   //OUTPUT: show movement downwards
-int pin8 = 8;     //OUTPUT: show movement to the left
+int pin12 = 12;   //OUTPUT: show tilted to the right
+int pin11 = 11;   //OUTPUT: show tilted forward
+int pin10 = 10;   //OUTPUT: show tilted backwards
+int pin8 = 8;     //OUTPUT: show left to the left
 
-int pin7 = 7;     //INPUT: test movement to the right
-int pin6 = 6;     //INPUT: test movement upwards
-int pin5 = 5;     //INPUT: test movement downwards
-int pin4 = 4;     //INPUT: test movement to the left
+int pin7 = 7;     //INPUT: test tilted to the right
+int pin6 = 6;     //INPUT: test tilted forward
+int pin5 = 5;     //INPUT: test tilted backwards
+int pin4 = 4;     //INPUT: test tilted to the left
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,32 +23,32 @@ void setup() {
 }
 
 void loop() {
-  if(digitalRead(pin7)){      //show right
-    digitalWrite(pin12, LOW);
+  if(digitalRead(pin7)){        //if tilt switch that tests right is reading high
+    digitalWrite(pin12, LOW);   //not tilted right
   }
   else{
-    digitalWrite(pin12, HIGH);
+    digitalWrite(pin12, HIGH);  //tilted the right
   }
 
-  if(digitalRead(pin6)){      //show up
-    digitalWrite(pin11, LOW);
+  if(digitalRead(pin6)){       //if tilt switch that tests forward is reading high
+    digitalWrite(pin11, LOW);   //not tilted forward
   }
   else{
-    digitalWrite(pin11, HIGH);
+    digitalWrite(pin11, HIGH);  //if tilt switch that tests backward is reading high
   }
 
   if(digitalRead(pin5)){      //show down
-    digitalWrite(pin10, LOW);
+    digitalWrite(pin10, LOW); //not tilted backward
   }
   else{
-    digitalWrite(pin10, HIGH);
+    digitalWrite(pin10, HIGH); //tilted backward
   }
 
-  if(digitalRead(pin4)){      //show left
-    digitalWrite(pin8, LOW);
+  if(digitalRead(pin4)){       //if tilt switch that tests left is reading high
+    digitalWrite(pin8, LOW);   //not tilted left
   }
   else{
-    digitalWrite(pin8, HIGH);
+    digitalWrite(pin8, HIGH); //tilted left
   }
   
 }
