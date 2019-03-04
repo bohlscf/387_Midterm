e5acb7f913e35ddba1a6b720f45baee32a5fa151
bohlscf <44482641+bohlscf@users.noreply.github.com>
2019-03-03 21:32:54 -0500

    Update Tilt_Memory.ino

--- Tilt_Memory.ino
+++ Tilt_Memory.ino
@@ -31,16 +31,17 @@ void setup() {
 }
 void loop() {
   
-   
+  //This sets the LEDs back to low and sets wait = 0 (meaning it is ready to read a new tilt direction)
   if(digitalRead(pin7) && digitalRead(pin6) && digitalRead(pin5)&& digitalRead(pin4)){
     wait = 0;
     digitalWrite(pin12,LOW);
     digitalWrite(pin11,LOW);
     digitalWrite(pin10,LOW);
     digitalWrite(pin8,LOW);
-    delay(150);
+    delay(150);               //This delay prevents extra counting on the tilts
     }
   
+  //This detects tilting to the right, only when the wait = 0. It also increases the 'right' counter
   if(digitalRead(pin7) == LOW && digitalRead(pin6) && digitalRead(pin5)&& digitalRead(pin4) && wait == 0){
     right++;
     digitalWrite(pin12,HIGH);
@@ -48,6 +49,7 @@ void loop() {
     delay(400);
   }
 
+  //This detects tilting up, only when the wait = 0. It also increases the 'up' counter
   if(digitalRead(pin7) && digitalRead(pin6) == LOW && digitalRead(pin5) && digitalRead(pin4) && wait == 0){
     up++;
     digitalWrite(pin11,HIGH);
@@ -55,6 +57,7 @@ void loop() {
     delay(400);
   }  
 
+  //This detects tilting down, only when the wait = 0. It also increases the 'down' counter
   if(digitalRead(pin7) && digitalRead(pin6)&& digitalRead(pin5) == LOW  && digitalRead(pin4) && wait == 0){
     down++;
     digitalWrite(pin10,HIGH);
@@ -62,6 +65,7 @@ void loop() {
     delay(400);
   }  
 
+  //This detects tilting to the left, only when the wait = 0. It also increases the 'left' counter
   if(digitalRead(pin7) && digitalRead(pin6) && digitalRead(pin5) && digitalRead(pin4) == LOW  && wait == 0){
     left++;
     digitalWrite(pin8,HIGH);
@@ -69,7 +73,9 @@ void loop() {
     delay(400);
   }   
 
+   //This enters the if statement when the button is pressed
    if(digitalRead(pin2)){
+    //All the LEDs light up for half a second
     digitalWrite(pin12,HIGH);
     digitalWrite(pin11,HIGH);
     digitalWrite(pin10,HIGH);
@@ -81,35 +87,37 @@ void loop() {
     digitalWrite(pin8,LOW);
     delay(300);
     
+    //Each LED lights up the specific amount of times, according to the direction counter
     while(right > 0){
       digitalWrite(pin12,HIGH);
       delay(400);
       digitalWrite(pin12,LOW);
       delay(300);
-      right--;
+      right--;                  //The counter decreases until it is back to zero
     }
     while(up > 0){
       digitalWrite(pin11,HIGH);
       delay(400);
       digitalWrite(pin11,LOW);
       delay(300);
-      up--;
+      up--;                     //The counter decreases until it is back to zero
     }
      while(down > 0){
       digitalWrite(pin10,HIGH);
       delay(400);
       digitalWrite(pin10,LOW);
       delay(300);
-      down--;
+      down--;                  //The counter decreases until it is back to zero
     }
      while(left > 0){
       digitalWrite(pin8,HIGH);
       delay(400);
       digitalWrite(pin8,LOW);
       delay(300);
-      left--;
+      left--;                  //The counter decreases until it is back to zero
     }
  
+    //The LEDs light up in order from right, to up, to down, and then to left
     digitalWrite(pin12,HIGH);
     delay(75);
     digitalWrite(pin11,HIGH);
